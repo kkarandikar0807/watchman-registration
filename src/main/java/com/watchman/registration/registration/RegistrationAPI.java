e6525cb98dd188464ad064a6230f242691cdd497
Karandikar, Kaushik <Kaushik.Karandikar@mastercard.com>
2018-02-22 11:53:08 -0500

    Tested hystrix to make it run

--- src/main/java/com/watchman/registration/registration/RegistrationAPI.java
+++ src/main/java/com/watchman/registration/registration/RegistrationAPI.java
@@ -3,6 +3,7 @@ package com.watchman.registration.registration;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.watchman.registration.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,20 +12,21 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 @RestController
+@EnableCircuitBreaker
 public class RegistrationAPI {
 
     @Autowired
     RegistrationService service;
 
 
-    @HystrixCommand(fallbackMethod = "test")
+    @HystrixCommand(defaultFallback = "fallbackHystrix")
     @PostMapping(path = "/register")
     public String registerUser(@RequestBody User user) throws IOException, URISyntaxException {
 
        return service.registerUser(user);
     }
 
-    public String test() {
+    public String fallbackHystrix() {
         return "Fallback method called";
     }
 }
--- src/main/java/com/watchman/registration/registration/RegistrationService.java
+++ src/main/java/com/watchman/registration/registration/RegistrationService.java
@@ -1,19 +1,22 @@
 package com.watchman.registration.registration;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.watchman.registration.user.User;
 import constants.ConstantRoutes;
 import helpers.UrlBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import java.io.IOException;
 import java.net.*;
 
-@Component
+@Service
 public class RegistrationService {
 
     @Value("${url}")
     private String url;
+
 
     protected String registerUser(User user) throws IOException, URISyntaxException {
         String endpoint = ConstantRoutes.register;
